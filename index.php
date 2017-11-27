7d4a3f5b675b24f3fce6b3d1824f9e7788b10862
Wisley José Alves <wisley@Lenovo-G470.ufop.br>
2017-11-27 14:18:52 -0200

    commit 2

--- index.php
+++ index.php
@@ -13,17 +13,38 @@ for ($i=0; $i <count($continent) ; $i++) {
   createContinentNeo4j($continent[$i]);
   $fileCountry = file_get_contents("http://www.geonames.org/childrenJSON?geonameId=".$continent[$i]["geonameId"]);
   $strC = json_decode($fileCountry,true);
+  if ($strC['geonames'] !== array()){ // if it is not empty
   $country = $strC['geonames'];
   // execute function to create node country and relationship
   createCountry($country,$continent[$i]["geonameId"]);
+  }
 }
 
 function createCountry($country,$continentId){ // function to create Node Country
-// var country all country in the continet
+// var country  = all country in the continet
 // for each country
 // execute function to create node country and relationship in Neo4j
   for ($i=0; $i <count($country) ; $i++) {
     createCountryNeo4j($country[$i],$continentId); //
+    $fileState = file_get_contents("http://www.geonames.org/childrenJSON?geonameId=".$country[$i]["geonameId"]);
+    $strS = json_decode($fileState,true);
+    // add condition if there are no states in the country
+    if ($strS['geonames'] !== array()){ // if it is not empty
+      $state = $strS['geonames'];
+      // execute function to create node country and relationship
+      createStates($state,$country[$i]["geonameId"]);
+    }
+
+
+  }
+}
+function createStates($states,$countryId){ // function to create Node Sates
+// var states = all states in the country
+// for each states
+// execute function to create node states and relationship in Neo4j
+  for ($i=0; $i <count($states) ; $i++) {
+    //echo $states[$i]["countryId"]."\n";
+    createStatesNeo4j($states[$i],$countryId); //
   }
 }
  ?>
--- neo4j.php
+++ neo4j.php
@@ -23,14 +23,27 @@ function createContinentNeo4j($continent){ // function to create continent in Ne
 function createCountryNeo4j($country,$continentId){ // function to create country in Neo4J
   $client = $GLOBALS['client']; // retrieves value from global client variable and saves to a local client variable
    $query = "MERGE (n:Country { id: {id}, name: {name},continentId:{continentId} }) RETURN n";
-   //cypher to create Conuntry Node with id property and name
+   //cypher to create Country Node with id property and name
    $parameters = array('id' => $country["geonameId"], 'name' => $country["name"],'continentId'=>$continentId );
    $client->sendCypherQuery($query, $parameters);// function to create Node by passing the value of the cypher and the parameters
    // execute other query for create RelationShip
-   $query2 = 'MATCH (c:Continent {id:{idContinent}}),(c2:Country {id:{idCountry}}) MERGE (c)-[:BelongTo]->(c2)';
+   $query2 = 'MATCH (c:Continent),(c2:Country) WHERE c.id = c2.continentId MERGE (c)-[:BelongTo]->(c2)';
    //Cypher to create Relationship (BelongTo) for Continent and Country
-   $parameters2 = array('idCountry' => $country["geonameId"], 'idContinent'=>$continentId );// parameters to create node
-   $client->sendCypherQuery($query2, $parameters2);// function to create Node by passing the value of the cypher and the parameters
+   $client->sendCypherQuery($query2);// function to create Node by passing the value of the cypher
+
+}
+
+function createStatesNeo4j($states,$countryID){
+  $client = $GLOBALS['client']; // retrieves value from global client variable and saves to a local client variable
+   $query = "MERGE (n:State { id: {id}, name: {name},countryId:{countryId} }) RETURN n";
+   //cypher to create State Node with id property and name
+   $parameters = array('id' => $states["geonameId"], 'name' => $states["name"],'countryId'=>$countryID );
+   $client->sendCypherQuery($query, $parameters);// function to create Node by passing the value of the cypher and the parameters
+
+   // execute other query for create RelationShip
+   $query2 = 'MATCH (c:Country),(c2:State) WHERE c.id = c2.countryId MERGE (c)-[:BelongTo]->(c2)';
+   //Cypher to create Relationship (BelongTo) for State and Country
+   $client->sendCypherQuery($query2);// function to create Node by passing the value of the cypher 
 
 }
 
