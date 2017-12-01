1f60b5f6533cf5c405f864af93594250ef84edc7
Wisley José Alves <wisley@Lenovo-G470.ufop.br>
2017-12-01 15:48:13 -0200

    update,add function delete

--- index.php
+++ index.php
@@ -24,19 +24,21 @@ return $varRes;
  require_once 'neo4j.php';
  // http://www.geonames.org/childrenJSON?geonameId=3469034 // brazil
  // http://www.geonames.org/childrenJSON?geonameId=6295630 // continents
+function addBd(){
+  $str = getUrl("http://www.geonames.org/childrenJSON?geonameId=6295630");
+  $continent = $str['geonames'];
 
-$str = getUrl("http://www.geonames.org/childrenJSON?geonameId=6295630");
-$continent = $str['geonames'];
-
-// for each continet
-// execute function to create node continent
-for ($i=0; $i <count($continent) ; $i++) {
-  createContinentNeo4j($continent[$i]);
-  $strC = getUrl("http://www.geonames.org/childrenJSON?geonameId=".$continent[$i]["geonameId"]);
-  if ($strC['geonames'] !== array()){ // if it is not empty
-  $country = $strC['geonames'];
-  // execute function to create node country and relationship
-  createCountry($country,$continent[$i]["geonameId"]);
+  // for each continet
+  // execute function to create node continent
+  for ($i=0; $i <count($continent) ; $i++) {
+    echo "****** ".$continent[$i]['name']." ****** \n";
+    createContinentNeo4j($continent[$i]);
+    $strC = getUrl("http://www.geonames.org/childrenJSON?geonameId=".$continent[$i]["geonameId"]);
+    if (is_array($strC['geonames'])){ // if it is not empty
+    $country = $strC['geonames'];
+    // execute function to create node country and relationship
+    createCountry($country,$continent[$i]["geonameId"]);
+    }
   }
 }
 
@@ -46,9 +48,10 @@ function createCountry($country,$continentId){ // function to create Node Countr
 // execute function to create node country and relationship in Neo4j
   for ($i=0; $i <count($country) ; $i++) {
     createCountryNeo4j($country[$i],$continentId); //
+    echo "--- ".$country[$i]['name']." --- \n";
     $strS  = getUrl("http://www.geonames.org/childrenJSON?geonameId=".$country[$i]["geonameId"]);
     // add condition if there are no states in the country
-    if ($strS['geonames'] !== array()){ // if it is not empty
+    if (is_array($strS['geonames'])){ // if it is not empty
       $state = $strS['geonames'];
       // execute function to create node country and relationship
       createStates($state,$country[$i]["geonameId"]);
@@ -63,7 +66,40 @@ function createStates($states,$countryId){ // function to create Node Sates
 // execute function to create node states and relationship in Neo4j
   for ($i=0; $i <count($states) ; $i++) {
     //echo $states[$i]["countryId"]."\n";
+    echo $states[$i]['toponymName']."\n";
     createStatesNeo4j($states[$i],$countryId); //
   }
 }
+function deleteNotExists(){
+  $all = getContinentNeo4j(/*$idContinet*/); // get all continent with id and name property
+  $str = getUrl("http://www.geonames.org/childrenJSON?geonameId=6295630");
+  $continent = $str['geonames'];
+  // for each continet
+  // create an object for comparison
+  $res =[]; // array of objects that still exists, comparison array
+  for ($i=0; $i <count($continent) ; $i++) {
+      $sname = $continent[$i]['name']; //
+      $sid = $continent[$i]['geonameId']; //
+      array_push($res,array('id' => $sid , 'name' => $sname )); // add id and name in object
+  }
+  $idDel =[]; // array with id of objects that no longer exist DB
+  for ($i=0; $i <count($all) ; $i++) {
+    if (in_array($all[$i], $res) === FALSE) { // comparison all (object array in DB) with res (object array that still exists)
+      // echo "YES ".$all[$i]["name"]."\n";
+      array_push($idDel,$all[$i]['id']); // Add the ID of the object that can no longer exist in the DB
+    }
+  }
+  if(empty($idDel)){ // check if there is an id
+    echo "DB already updated"."\n";
+  }
+  else {
+    // for each id, execute the function to delete the NODE
+    for ($i=0; $i <count($idDel) ; $i++) {
+      deleteByIdNeo4j($idDel[$i]); // execute function to delete NODE in Neo4j
+    }
+  }
+}
+//execute function
+//addBd();
+deleteNotExists();
  ?>
--- neo4j.php
+++ neo4j.php
@@ -1,21 +1,20 @@
 <?php
 /* exemple php whit neo4j */
 require_once 'vendor/autoload.php';
-
 use Neoxygen\NeoClient\ClientBuilder;// add libary Neo4J
 
 $client = ClientBuilder::create() // create connection Neo4j
     ->addConnection('default', 'http', 'localhost', 7474, true, 'wis', 'neo4j') // initial connection username :wis password:neo4j
+    ->setAutoFormatResponse(true)
     ->build();
-
-$version = $client->getNeo4jVersion();
+//$version = $client->getNeo4jVersion();
 //  echo $version;
 
 //var_dump($response);
 function createContinentNeo4j($continent){ // function to create continent in Neo4J
   $client = $GLOBALS['client']; // retrieves value from global client variable and saves to a local client variable
    $query = "MERGE (n:Continent { id: {id}, name: {name} }) RETURN n"; // cypher to create Continent Node with id property and name
-   $parameters = array('id' => $continent["geonameId"], 'name' => $continent["toponymName"] ); // parameters to create node
+   $parameters = array('id' => $continent["geonameId"], 'name' => $continent["name"] ); // parameters to create node
    $client->sendCypherQuery($query, $parameters); // function to create Node by passing the value of the cypher and the parameters
 
 }
@@ -24,7 +23,7 @@ function createCountryNeo4j($country,$continentId){ // function to create countr
   $client = $GLOBALS['client']; // retrieves value from global client variable and saves to a local client variable
    $query = "MERGE (n:Country { id: {id}, name: {name},continentId:{continentId} }) RETURN n";
    //cypher to create Country Node with id property and name
-   $parameters = array('id' => $country["geonameId"], 'name' => $country["toponymName"],'continentId'=>$continentId );
+   $parameters = array('id' => $country["geonameId"], 'name' => $country["name"],'continentId'=>$continentId );
    $client->sendCypherQuery($query, $parameters);// function to create Node by passing the value of the cypher and the parameters
    // execute other query for create RelationShip
    $query2 = 'MATCH (c:Continent),(c2:Country) WHERE c.id = c2.continentId MERGE (c)-[:HasCountry]->(c2)';
@@ -45,6 +44,54 @@ function createStatesNeo4j($states,$countryID){
    //Cypher to create Relationship (BelongTo) for State and Country
    $client->sendCypherQuery($query2);// function to create Node by passing the value of the cypher
 
+
 }
 
+function getContinentNeo4j(/* $idContinet */){
+  $client = $GLOBALS['client']; // retrieves value from global client variable and saves to a local client variable
+   $query = 'MATCH (c:Continent) RETURN c ';
+   //cypher to ..
+  $result =$client->sendCypherQuery($query);// function to create Node by passing the value of the cypher and the parameters
+  $publicResult = $result->getBody(); // get public reponse, because $result is protected
+  $response = $publicResult["results"][0]["data"];
+  // filtering results
+  /* exemple of filtering */
+  $res = [];
+  for ($i=0; $i < count($response) ; $i++) {
+    $sname = $response[$i]["row"][0]["name"];
+    $sid = $response[$i]["row"][0]['id'];
+    //echo $sname."\n".$sid ."\n" ; // print name and id
+      array_push($res,array('id' => $sid , 'name' => $sname ));
+  }
+  return $res; // return result
+}
+
+function deleteByIdNeo4j($ID){
+  echo "Delete node for id: ".$ID."\n";
+  $client = $GLOBALS['client']; // retrieves value from global client variable and saves to a local client variable
+  $parameters = array('idC' => $ID ); // parametrs for seach
+  //***************** search continent by id, search country by continentId and search a country relationship with states
+  $q1 = 'MATCH (c:Continent {id:{idC}}) MATCH (c1:Country {continentId:c.id})-[rs:HasState]->()';
+  $d1 = 'DELETE rs'; // delete relationship
+  //***************** search continent by id, search country by continentId and search a continent relationship with country
+  $q2 = 'MATCH (c:Continent {id:{idC}})-[rc:HasCountry]->()';
+  $d2 = 'DELETE rc';// delete relationship
+  //***************** search continent by id, search country by continentId and search state by countryId
+  $q3 = 'MATCH (c:Continent {id:{idC}}) MATCH (c1:Country {continentId:c.id}) MATCH (s:State {countryId:c1.id})';
+  $d3 = 'DELETE s'; // delete state
+  //***************** search continent by id and search country by continentId
+  $q4 = 'MATCH (c:Continent {id:{idC}}) MATCH (c1:Country {continentId:c.id})';
+  $d4 = 'DELETE c1'; // delete country
+  //***************** search continent by id
+  $q5 = 'MATCH (c:Continent {id:{idC}})';
+  $d5 = 'DELETE c'; // delete continent
+  //*****************
+  $client->sendCypherQuery($q1.$d1, $parameters); // execute query with parametrs
+  $client->sendCypherQuery($q2.$d2, $parameters); // execute query with parametrs
+  $client->sendCypherQuery($q3.$d3, $parameters); // execute query with parametrs
+  $client->sendCypherQuery($q4.$d4, $parameters); // execute query with parametrs
+  $client->sendCypherQuery($q5.$d5, $parameters); // execute query with parametrs
+  //*****************
+
+}
  ?>
